2a4786350dede3c4172dcd2e8dd6fdf859525344
Taras-Roboteckyi <rtvr@ukr.net>
2024-11-20 22:35:43 +0200

    bag in select styled

--- src/components/Input/Select/Select.jsx
+++ src/components/Input/Select/Select.jsx
@@ -1,11 +1,17 @@
 export const Select = ({ value, onChange, option, text }) => {
   const { first, second, third } = option;
-
+  /* bg-bg_color */
   return (
     <label className="mb-3 ">
       {text}:
-      <select value={value} onChange={onChange} className="bg-bg_color">
-        <option value={first}>{first}</option>
+      <select
+        value={value}
+        onChange={onChange}
+        className="border-2 border-red-500 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-400"
+      >
+        <option value={first} className="border-2 border-rose-600">
+          {first}
+        </option>
         <option value={second}>{second}</option>
         {third && <option value={third}>{third}</option>}
       </select>
