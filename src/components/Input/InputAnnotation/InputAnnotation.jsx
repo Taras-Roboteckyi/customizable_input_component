9832f468dc897ef6dc8071df004cc7db7aba5112
Taras-Roboteckyi <rtvr@ukr.net>
2024-12-23 22:55:53 +0200

    refactoring InputAnnotations code

--- src/components/Input/InputAnnotation/InputAnnotation.jsx
+++ src/components/Input/InputAnnotation/InputAnnotation.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { useState } from "react";
 
 import { BsInfoCircle } from "react-icons/bs";
 
 const InputAnnotation = ({ text, ...props }) => {
-  /* const [isHovered, setIsHovered] = useState(false); */
+  const mouseEnter = () =>
+    (document.getElementById("text").style.display = "inline");
+  const mouseLeave = () =>
+    (document.getElementById("text").style.display = "none");
+
   return (
-    <div className="flex items-center space-x-2">
-      <div
-        className="group flex items-center space-x-2"
-        onMouseEnter={() =>
-          (document.getElementById("text").style.display = "inline")
-        }
-        onMouseLeave={() =>
-          (document.getElementById("text").style.display = "none")
-        }
-      >
-        <BsInfoCircle className="text-gray-700 group-hover:text-blue-500" />
-        <span id="text" className="text-gray-700 hidden">
-          Текст підсвічується
-        </span>
-      </div>
+    <div
+      className="group flex items-center space-x-2"
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+    >
+      <BsInfoCircle className="text-gray-700 group-hover:text-blue-500" />
+      <span id="text" className="font-sans text-sm text-gray-700 hidden">
+        This is tooltip
+      </span>
     </div>
   );
 };
