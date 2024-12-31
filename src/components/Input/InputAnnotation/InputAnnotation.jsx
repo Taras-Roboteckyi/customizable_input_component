4cb497289347e42232b3f149daf3fdaf08a5b9d0
Taras-Roboteckyi <rtvr@ukr.net>
2024-12-31 21:47:18 +0200

    added tooltip with white color

--- src/components/Input/InputAnnotation/InputAnnotation.jsx
+++ src/components/Input/InputAnnotation/InputAnnotation.jsx
@@ -1,27 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
 import { BsInfoCircle } from "react-icons/bs";
 
 const InputAnnotation = ({ text, ...props }) => {
-  const mouseEnter = () =>
+  /* const mouseEnter = () =>
     (document.getElementById("text").style.display = "inline");
   const mouseLeave = () =>
-    (document.getElementById("text").style.display = "none");
+    (document.getElementById("text").style.display = "none"); */
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
+  const mouseEnter = () => {
+    setIsTooltipVisible(true);
+  };
+
+  const mouseLeave = () => {
+    setIsTooltipVisible(false);
+  };
 
   return (
     <div
-      className="group relative flex items-center space-x-2"
+      className="relative flex items-center space-x-2"
       onMouseEnter={mouseEnter}
       onMouseLeave={mouseLeave}
     >
-      <BsInfoCircle className="text-gray-700 group-hover:text-blue-500" />
-      <span
-        id="text"
-        className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 hidden w-max px-3 py-1 text-sm text-white bg-black rounded shadow group-hover:block"
-      >
-        This is tooltip
-        <div className="absolute left-1/2 -translate-x-1/2 top-full border-4 border-black border-transparent"></div>
-      </span>
+      <BsInfoCircle className="text-gray-700 text-2xl cursor-pointer" />
+      {isTooltipVisible && (
+        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2">
+          <div className="relative bg-white text-black text-sm px-3 py-1 rounded shadow-lg">
+            This is tooltip
+            {/* Трикутник */}
+            <div className="absolute left-1/2 transform -translate-x-1/2 top-full w-3 h-3 bg-white rotate-45"></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
