8755094a40eb7ffa7d503e5b1004d6406199d398
Taras-Roboteckyi <rtvr@ukr.net>
2025-03-25 21:44:12 +0200

    refactoring code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { ThemeProvider } from "styled-components";
+//import { ThemeProvider } from "styled-components";
 
 import App from "./App";
 
-import { Constants } from "./constans/Constans";
-
+//import { Constants } from "./constans/Constans";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={Constants}>
-     
-      <App />
-    </ThemeProvider>
+    {/* <ThemeProvider theme={Constants}> */}
+    <App />
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
