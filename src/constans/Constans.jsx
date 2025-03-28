dd0da5abaa72c1fd3424e38daa01f836d9e303f2
Taras-Roboteckyi <rtvr@ukr.net>
2025-03-28 21:11:45 +0200

    cleaned commites

--- src/constans/Constans.jsx
+++
@@ -1,1 +0,0 @@
-const OpenSans = `'Open Sans'`;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//import { ThemeProvider } from "styled-components";
-
 import App from "./App";
-
-//import { Constants } from "./constans/Constans";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={Constants}> */}
     <App />
-    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
